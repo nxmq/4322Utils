3fab15796d94c835593c07450b09de3226310b69
Nicolas Machado <nxmachado@wpi.edu>
2019-01-02 16:51:37 -0800

    More ramsete fixes.

--- src/org/usfirst/frc/team4322/motion/RamseteController.kt
+++ src/org/usfirst/frc/team4322/motion/RamseteController.kt
@@ -29,16 +29,15 @@ class RamseteController(val path: Trajectory, val robotWheelBase: Double, val kb
             return Pair(0.0, 0.0)
         if (initMod.isNaN()) {
             initMod = Math.round((RobotPositionIntegrator.lastPose.theta - (path[0].heading + Math.PI)) / (2.0 * Math.PI)) * 2.0 * Math.PI
+            if (path[seg].heading + initMod + Math.PI - lastGoalAngle >= Math.PI) {
+                initMod -= path[seg].heading + initMod + Math.PI - lastGoalAngle
+            }
         }
 
-        if (path[seg].heading + initMod + Math.PI - lastGoalAngle >= Math.PI) {
-            initMod -= path[seg].heading + initMod + Math.PI - lastGoalAngle
-        }
 
         val goalAngle = path[seg].heading + initMod + Math.PI
         // This is the path vel and can be used for vd
         val goalVelocity = -path[seg].velocity
-
 
         val goalAngleDelta = (goalAngle - lastGoalAngle) / path[seg].dt
         lastGoalAngle = goalAngle
@@ -67,7 +66,7 @@ class RamseteController(val path: Trajectory, val robotWheelBase: Double, val kb
          * Here she is! Equation 5.12. if you notice while reading the white paper equation 5.12 is wrong... it improperly implements linear projection
          * the correct implementation is (Math.cos(angle) * (gy - ry) - Math.sin(angle) * (gx - rx)) in the ramw
          */
-        val angleError = goalAngle - angle
+        val angleError = Math.max(goalAngle - angle, Math.nextUp(0.0))
         val ramv = goalVelocity * Math.cos(angleError) + k1 * (Math.cos(angle) * xError + Math.sin(angle) * yError)
         var ramw = goalAngleDelta + kb * goalVelocity * (Math.sin(angleError) / angleError) * (Math.cos(angle) * yError - Math.sin(angle) * xError) + k1 * angleError
 
